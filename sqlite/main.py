4222e9f2f6244ca0d4ea7ae7debb4de118c561a6
EduardoRamilloLeite <eduardoramilloleite@leite.com>
2024-10-22 12:27:27 -0300

    creating table

--- sqlite/main.py
+++ sqlite/main.py
@@ -1,10 +1,21 @@
 import sqlite3
 from pathlib import Path
+
 ROOT_DIR = Path(__file__).parent
 DB_NAME = 'db.sqlite3'
 DB_FILE = ROOT_DIR / DB_NAME
+TABLE_NAME = 'customers'
+
 connection = sqlite3.connect(DB_FILE)
-cursor = connection.cursor()
-# SQL
+cursor = connection.cursor() #vai executar as coisa dentro do DB
+
+cursor.execute( f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+               '('
+               'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+               'name TEXT,'
+               'weight REAL'
+               ')'
+)
+
 cursor.close()
 connection.close()
